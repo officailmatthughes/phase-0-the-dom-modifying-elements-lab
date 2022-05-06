5f64b764552284c315f499bbcc65e029b338de5f
Mattherw <officialmatthughes>
2022-05-05 19:04:38 -0700

    commit tree 9ac6a50de3e2791be4f20b325918a1e0cc7f13fa

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const body = document.getElementsByTagName("body")[0];
+body.innerHTML =
+  '<h1 id="victory">is the champion</h1>';
+let newHeader = document.getElementById("victory");
